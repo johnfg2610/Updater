5d4398cbf146c3c84a4e80a41dd31117207bf5da
Unknown <johnfg2610@gmail.com>
2016-12-11 10:51:02 +0000

    Added a test console class

---
+++ TestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Updater;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var client = new WebClient())
+            {
+                Console.WriteLine(client.DownloadString("https://raw.githubusercontent.com/Johnfg12/Updater/master/Version"));
+            }
+        }
+    }
+}
--- Updater/Class1.cs
+++ Updater/Class1.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Net;
 
 namespace Updater
 {
     public class Class1
     {
-        public void CheckSorce(Uri location)
+        public void CheckSorce(string location)
         {
-
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("https://raw.githubusercontent.com/Johnfg12/Updater/master/Version", "txt");
+            }
         }
     }
 }
